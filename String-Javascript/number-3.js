75a82ee54329ab3426e8da4d26c8b2e5dd6de1ea
viutytiadita <tviuty1206@gmail.com>
2019-03-08 13:36:02 +0700

    exercises5-bermain dengan string

---
+++ String-Javascript/number-3.js
@@ -0,0 +1,12 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14);
+var thirdWord3 = word.substring(15, 17);
+var fourthWord3 = word.substring(18, 20);
+var fifthWord3 = word.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
---
+++ String-Javascript/number-4.js
@@ -0,0 +1,20 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14);
+var thirdWord3 = word.substring(15, 17);
+var fourthWord3 = word.substring(18, 20);
+var fifthWord3 = word.substring(21, 25);
+
+var firstWordLength = exampleFirstWord3.length;
+var secWordLength = secondWord3.length;
+var thirdWordLength = thirdWord3.length;
+var fourthWordLength = fourthWord3.length;
+var fifthWordLength = fifthWord3.length;
+
+
+console.log('First Word: ' + exampleFirstWord3 + ', with length:'+firstWordLength);
+console.log('Second Word: ' + secondWord3+', with length:'+secWordLength);
+console.log('Third Word: ' + thirdWord3+ ', with length:'+thirdWordLength);
+console.log('Fourth Word: ' + fourthWord3+ ', with length:'+fourthWordLength);
+console.log('Fifth Word: ' + fifthWord3+ ', with length:'+fifthWordLength);
+
